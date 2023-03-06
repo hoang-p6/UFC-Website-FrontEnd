aaae7a7ac1cc2f286afba0f130bc639b51d47d37
Phong Hoang <p.hoang6900@gmail.com>
2023-03-06 14:07:35 -0500

    save for merge

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,7 +1,23 @@
 import Card from './Card'
+import { GetCards } from '../services/CardServices'
+import { useEffect, useState } from 'react'
 
 const Home = () => {
-  return <div></div>
+  const [cards, setCards] = useState([])
+  useEffect(() => {
+    const handleCards = async () => {
+      const data = await GetCards()
+      setCards(data)
+    }
+    handleCards()
+  }, [])
+  return (
+    <div>
+      {cards.map((card) => (
+        <h1>{card.title}</h1>
+      ))}
+    </div>
+  )
 }
 
 export default Home

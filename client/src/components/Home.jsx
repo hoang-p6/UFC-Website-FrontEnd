2710496cf8e100ccc8ac5abd4bfdd9aa00718aec
Eduardo Puerta <puerta.eduardo10@gmail.com>
2023-03-06 16:04:26 -0500

    home page mapped

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,21 +1,35 @@
-import Card from './Card'
+
 import { GetCards } from '../services/CardServices'
 import { useEffect, useState } from 'react'
 
 const Home = () => {
   const [cards, setCards] = useState([])
-  useEffect(() => {
-    const handleCards = async () => {
+  
+  useEffect(()=> {
+    const handleCards = async ()=> {
       const data = await GetCards()
       setCards(data)
     }
     handleCards()
   }, [])
+  
   return (
-    <div>
-      {cards.map((card) => (
-        <h1>{card.title}</h1>
-      ))}
+    <div className="Main-Card">
+      <h1 className='Title'>UFC HUB</h1>
+      {cards.map((cards) => (
+        <div className='Card-main' key={cards.id}>
+          <div className='Card-Div'>
+            <h2 className='Card-Title'>{cards.title}</h2>
+            <img src='https://www.insidesport.in/wp-content/uploads/2023/01/WhatsApp-Image-2023-01-15-at-1.44.18-PM.jpeg' alt='JonesvGane'/>
+            <p>Date: {cards.date}</p>
+            <p>Time: {cards.startTime}</p>
+            <p>City: {cards.city}</p>
+            <p>Country: {cards.country}</p>
+            <p>Arena: {cards.arena}</p>
+          </div>
+          </div>
+      ))
+      }
     </div>
   )
 }
--- client/src/services/CardServices.js
+++ client/src/services/CardServices.js
@@ -2,7 +2,7 @@ import Client from './api'
 
 export const GetCards = async () => {
   try {
-    const res = await Client.get('/cards')
+    const res = await Client.get('/cards/list')
     return res.data
   } catch (error) {
     throw error

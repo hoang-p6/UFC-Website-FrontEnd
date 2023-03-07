0902e133ac0971105c79c16f914e361b41c684cf
Phong Hoang <p.hoang6900@gmail.com>
2023-03-07 09:41:05 -0500

    Save for pull

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,35 +1,33 @@
-
 import { GetCards } from '../services/CardServices'
 import { useEffect, useState } from 'react'
 
 const Home = () => {
   const [cards, setCards] = useState([])
-  
-  useEffect(()=> {
-    const handleCards = async ()=> {
+
+  useEffect(() => {
+    const handleCards = async () => {
       const data = await GetCards()
       setCards(data)
     }
     handleCards()
   }, [])
-  
+
   return (
     <div className="Main-Card">
-      <h1 className='Title'>UFC HUB</h1>
-      {cards.map((cards) => (
-        <div className='Card-main' key={cards.id}>
-          <div className='Card-Div'>
-            <h2 className='Card-Title'>{cards.title}</h2>
-            <img src='https://www.insidesport.in/wp-content/uploads/2023/01/WhatsApp-Image-2023-01-15-at-1.44.18-PM.jpeg' alt='JonesvGane'/>
-            <p>Date: {cards.date}</p>
-            <p>Time: {cards.startTime}</p>
-            <p>City: {cards.city}</p>
-            <p>Country: {cards.country}</p>
-            <p>Arena: {cards.arena}</p>
+      <h1 className="Title">UFC HUB</h1>
+      {cards.map((card) => (
+        <div className="Card-main" key={card.id}>
+          <div className="Card-Div">
+            <h2 className="Card-Title">{card.title}</h2>
+            <img src={card.image} alt="JonesvGane" />
+            <p>Date: {card.date.split('T')[0]}</p>
+            <p>Time: {card.startTime}</p>
+            <p>City: {card.city}</p>
+            <p>Country: {card.country}</p>
+            <p>Arena: {card.arena}</p>
           </div>
-          </div>
-      ))
-      }
+        </div>
+      ))}
     </div>
   )
 }

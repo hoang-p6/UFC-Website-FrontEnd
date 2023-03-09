6521d7317f0d0fdab9e2255fcf10a1c03a037e7b
odonovan1 <odogdonovan@gmail.com>
2023-03-09 17:16:56 -0500

    Added basic styling to card details page

--- client/src/components/CardDetails.jsx
+++ client/src/components/CardDetails.jsx
@@ -27,7 +27,7 @@ const CardDetails = ({ cards, getCards }) => {
     <div className='fightsAndDetails'>
       <div className='fightDetails'>
         <h1 className='cardTitle'>{thisCard.title}</h1>
-        <img src={thisCard.image} alt="card-image" className='' />
+        <img src={thisCard.image} alt="card-image" className='cardImage' />
         <div className='cardDetails'>
           <h2>{thisCard.startTime}</h2>
           <h2>{thisCard.arena}</h2>
@@ -38,28 +38,30 @@ const CardDetails = ({ cards, getCards }) => {
       </div>
       <div>
         <div>
+          <h1>Main Card</h1>
+        </div>
+        <div className='fightsOnCard'>
           {fighters.map((fightersDetails) => (
             <Link to={`/fightdetails/${fightersDetails[3]}`}>
               <div key={fightersDetails[0].firstName} className="eachFight">
-                <div className=''>
-
-                  <h1>
+                <div className='matchup'>
+                  <h1 className='names'>
                     {fightersDetails[0][0].firstName}{' '}
                     {fightersDetails[0][0].lastName}
                   </h1>
-                  <h2>VS</h2>
-                  <h1>
+                  <h1 className='vs'>VS</h1>
+                  <h1 className='names'>
                     {fightersDetails[0][1].firstName}{' '}
                     {fightersDetails[0][1].lastName}
                   </h1>
                 </div>
                 <div className='imageMatch'>
-                  <img src={fightersDetails[0][0].image} alt="fighterPicture" />
-                  <h1>VS.</h1>
-                  <img src={fightersDetails[0][1].image} alt="fighterPicture" />
+                  <img src={fightersDetails[0][0].image} alt="fighterPicture" className='fighterPicture' />
+                  <h1 className='vs'>VS.</h1>
+                  <img src={fightersDetails[0][1].image} alt="fighterPicture" className='fighterPicture' />
                 </div>
-                <h2>Division: {fightersDetails[1]}</h2>
-                <h2>Winner: {fightersDetails[2]}</h2>
+                <h2 className='divisionWinner'>Division: {fightersDetails[1]}</h2>
+                <h2 className='divisionWinner'>Winner: {fightersDetails[2]}</h2>
                 {/* <h2>Fight Id: {fightersDetails[3]}</h2> */}
                 {/* <h2>Card Id: {fightersDetails[4]}</h2> */}
               </div>

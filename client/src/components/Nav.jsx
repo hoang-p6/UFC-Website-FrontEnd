9b693ec649754e00fa4413adcc11fed9d1186416
odonovan1 <odogdonovan@gmail.com>
2023-03-09 14:51:48 -0500

    add styles folder to better display

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -1,42 +1,43 @@
 import { NavLink } from 'react-router-dom'
 import { useState, useEffect } from 'react'
 import axios from 'axios'
+import '../styles/Nav.css'
 
 const Nav = ({ user, handleLogout, checkToken }) => {
   let userOptions
 
-  const [userDetails, setUserDetails] = useState({})
+  // const [userDetails, setUserDetails] = useState({})
 
-  const getUserName = async () => {
-    const userId = user.id
+  // const getUserName = async () => {
+  //   const userId = user.id
 
-    const userStuff = await axios.get(`http://localhost:3001/auth/${userId}/details`)
-    setUserDetails(userStuff)
+  //   const userStuff = await axios.get(`http://localhost:3001/auth/${userId}/details`)
+  //   setUserDetails(userStuff)
 
 
-  }
-  console.log('HERE')
-  console.log(userDetails)
+  // }
+  // console.log('HERE')
+  // console.log(userDetails)
 
-  useEffect(() => {
-    getUserName()
-  }, [user])
+  // useEffect(() => {
+  //   getUserName()
+  // }, [user])
 
   return user ? (
-    <nav>
-      <NavLink to="/">Home</NavLink>
-      <NavLink to="/" onClick={handleLogout}>
+    <nav className='Nav-Bar'>
+      <NavLink to="/" className='Nav-Bar'>Home</NavLink>
+      <NavLink to="/" onClick={handleLogout} className='Nav-Bar'>
         Log Out
       </NavLink>
       {user.id === 1 &&
-        <NavLink to="/admincontrols">Admin Controls</NavLink>}
-      <h3>Welcome {userDetails.data.userName}!</h3>
+        <NavLink to="/admincontrols" className='Nav-Bar'>Admin Controls</NavLink>}
+      {/* <h3>Welcome {userDetails.data.userName}!</h3> */}
     </nav>
   ) : (
-    <nav>
-      <NavLink to="/">Home</NavLink>
-      <NavLink to="/register">Register</NavLink>
-      <NavLink to="/login">Login</NavLink>
+    <nav className='Nav-Bar'>
+      <NavLink to="/" className='Nav-Bar'>Home</NavLink>
+      <NavLink to="/register" className='Nav-Bar'>Register</NavLink>
+      <NavLink to="/login" className='Nav-Bar'>Login</NavLink>
 
     </nav>
   )

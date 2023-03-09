59d4d5a0aa85f06c6a76fec97d4164b2ce192a1f
odonovan1 <odogdonovan@gmail.com>
2023-03-09 12:07:21 -0500

    GITDADDY PHONG

--- client/src/components/CardDetails.jsx
+++ client/src/components/CardDetails.jsx
@@ -24,32 +24,40 @@ const CardDetails = ({ cards, getCards }) => {
   const fights = cards[id - 1].Fights
 
   return (
-    <div>
-      <div>
-        <h1>{thisCard.title}</h1>
-        <img src={thisCard.image} alt="card-image" />
-        <h2>{thisCard.startTime}</h2>
-        <h2>{thisCard.arena}</h2>
-        <h2>
-          {thisCard.city}, {thisCard.country}
-        </h2>
+    <div className='fightsAndDetails'>
+      <div className='fightDetails'>
+        <h1 className='cardTitle'>{thisCard.title}</h1>
+        <img src={thisCard.image} alt="card-image" className='' />
+        <div className='cardDetails'>
+          <h2>{thisCard.startTime}</h2>
+          <h2>{thisCard.arena}</h2>
+          <h2>
+            {thisCard.city}, {thisCard.country}
+          </h2>
+        </div>
       </div>
       <div>
         <div>
           {fighters.map((fightersDetails) => (
             <Link to={`/fightdetails/${fightersDetails[3]}`}>
               <div key={fightersDetails[0].firstName} className="eachFight">
-                <h1>
-                  {fightersDetails[0][0].firstName}{' '}
-                  {fightersDetails[0][0].lastName}
-                </h1>
-                <h2>VS</h2>
-                <h1>
-                  {fightersDetails[0][1].firstName}{' '}
-                  {fightersDetails[0][1].lastName}
-                </h1>
-                <img src={fightersDetails[0][0].image} alt="fighterPicture" />
-                <img src={fightersDetails[0][1].image} alt="fighterPicture" />
+                <div className=''>
+
+                  <h1>
+                    {fightersDetails[0][0].firstName}{' '}
+                    {fightersDetails[0][0].lastName}
+                  </h1>
+                  <h2>VS</h2>
+                  <h1>
+                    {fightersDetails[0][1].firstName}{' '}
+                    {fightersDetails[0][1].lastName}
+                  </h1>
+                </div>
+                <div className='imageMatch'>
+                  <img src={fightersDetails[0][0].image} alt="fighterPicture" />
+                  <h1>VS.</h1>
+                  <img src={fightersDetails[0][1].image} alt="fighterPicture" />
+                </div>
                 <h2>Division: {fightersDetails[1]}</h2>
                 <h2>Winner: {fightersDetails[2]}</h2>
                 {/* <h2>Fight Id: {fightersDetails[3]}</h2> */}
--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -2,22 +2,35 @@ import { NavLink } from 'react-router-dom'
 import { useState, useEffect } from 'react'
 import axios from 'axios'
 
-const Nav = ({ user, handleLogout, userDetails, checkToken }) => {
+const Nav = ({ user, handleLogout, checkToken }) => {
   let userOptions
 
+  const [userDetails, setUserDetails] = useState({})
+
+  const getUserName = async () => {
+    const userId = user.id
+
+    const userStuff = await axios.get(`http://localhost:3001/auth/${userId}/details`)
+    setUserDetails(userStuff)
 
 
+  }
+  console.log('HERE')
+  console.log(userDetails)
 
+  useEffect(() => {
+    getUserName()
+  }, [user])
 
   return user ? (
     <nav>
-      <h3>Welcome {userDetails}!</h3>
       <NavLink to="/">Home</NavLink>
       <NavLink to="/" onClick={handleLogout}>
         Log Out
       </NavLink>
       {user.id === 3 &&
         <NavLink to="/admincontrols">Admin Controls</NavLink>}
+      <h3>Welcome {userDetails.data.userName}!</h3>
     </nav>
   ) : (
     <nav>

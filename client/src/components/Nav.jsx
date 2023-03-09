e78bdf6891d5c5193c5aca7f4b7e708a664de7c4
Phong Hoang <p.hoang6900@gmail.com>
2023-03-09 14:55:53 -0500

    Save for pull

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -10,42 +10,41 @@ const Nav = ({ user, handleLogout, checkToken }) => {
   const getUserName = async () => {
     const userId = user.id
 
-    const userStuff = await axios.get(`http://localhost:3001/auth/${userId}/details`)
+    const userStuff = await axios.get(
+      `http://localhost:3001/auth/${userId}/details`
+    )
     setUserDetails(userStuff)
-
-
   }
-  console.log('HERE')
-  console.log(userDetails)
 
   useEffect(() => {
     getUserName()
   }, [user])
 
   return user ? (
-
-    <nav className='Nav-Bar'>
-      
-
+    <nav className="Nav-Bar">
       <h3>Welcome {userDetails}!</h3>
-      <NavLink className='Nav-Bar' to="/">Home</NavLink>
-      <NavLink className='Nav-Bar' to="/" onClick={handleLogout}>
-
+      <NavLink className="Nav-Bar" to="/">
+        Home
+      </NavLink>
+      <NavLink className="Nav-Bar" to="/" onClick={handleLogout}>
         Log Out
       </NavLink>
-      {user.id === 3 &&
-        <NavLink to="/admincontrols">Admin Controls</NavLink>}
+      {user.id === 3 && <NavLink to="/admincontrols">Admin Controls</NavLink>}
       <h3>Welcome {userDetails.data.userName}!</h3>
     </nav>
   ) : (
-    <nav className='Nav-Bar'>
-      <NavLink className='Nav-Bar' to="/">Home</NavLink>
-      <NavLink className='Nav-Bar' to="/register">Register</NavLink>
-      <NavLink className='Nav-Bar' to="/login">Login</NavLink>
-
+    <nav className="Nav-Bar">
+      <NavLink className="Nav-Bar" to="/">
+        Home
+      </NavLink>
+      <NavLink className="Nav-Bar" to="/register">
+        Register
+      </NavLink>
+      <NavLink className="Nav-Bar" to="/login">
+        Login
+      </NavLink>
     </nav>
   )
 }
- 
 
 export default Nav

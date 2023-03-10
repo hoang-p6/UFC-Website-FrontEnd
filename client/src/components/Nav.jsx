f91fdf21451ba7ff9860bed9ca5fe3fb0f5a6635
Phong Hoang <p.hoang6900@gmail.com>
2023-03-10 11:08:38 -0500

    Save for pull

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -24,25 +24,27 @@ const Nav = ({ user, handleLogout, checkToken }) => {
 
   return user ? (
     <nav className="Nav-Bar">
-
-      <div className='links'>
+      <div className="links">
         <NavLink to="/" className="Nav-Bar link">
-          Home
+          HOME
         </NavLink>
         <NavLink to="/" onClick={handleLogout} className="Nav-Bar link">
-          Log Out
+          LOG OUT
         </NavLink>
       </div>
-      <div>
-        <h1>UFC HUB</h1>
+      <div className="ufc-hub">
+        <h1 className="title">UFC</h1>
+        <h1 className="hub">HUB</h1>
       </div>
       <div>
         {user.id === 1 && (
           <NavLink to="/admincontrols" className="Nav-Bar link">
-            Admin Controls
+            <span className="material-symbols-outlined" id="settings-icon">
+              settings_input_component
+            </span>{' '}
+            <h1 className="admin">ADMIN</h1>
           </NavLink>
         )}
-
       </div>
     </nav>
   ) : (
@@ -54,7 +56,6 @@ const Nav = ({ user, handleLogout, checkToken }) => {
         Register
       </NavLink>
       <NavLink to="/login" className="Nav-Bar  link">
-
         Login
       </NavLink>
     </nav>
